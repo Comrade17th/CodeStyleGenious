69fcbc96212ce3524a905c24f91926639aec493c
nasusfen <ravshannedonat@gmail.com>
2024-02-04 17:06:52 +0300

    codeStyleAdded

--- GoPlaces.cs
+++ GoPlaces.cs
@@ -1,43 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GoPlaces : MonoBehaviour
 {
-    public float _float;
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _pointsParent;
 
+    private Transform[] _points;
+    private int _currentPoint;
 
+    private void Start() {
+        _points = new Transform[_pointsParent.childCount];
 
-    public Transform AllPlacespoint;
-    Transform[] arrayPlaces;
-    private int NumberOfPlaceInArrayPlaces;
-    void Start() {
-        arrayPlaces = new Transform[AllPlacespoint.childCount];
+        for (int i = 0; i < _pointsParent.childCount; i++)
+            _points[i] = _pointsParent.GetChild(i).GetComponent<Transform>();
+    }
 
-        for (int abcd = 0; abcd < AllPlacespoint.childCount; abcd++)
-            arrayPlaces[abcd] = AllPlacespoint.GetChild(abcd).GetComponent<Transform>();
-        }
-    // Update is called once per frame
-    public void Update()
+    private void Update()
     {
-        var _pointByNumberInArray= arrayPlaces[NumberOfPlaceInArrayPlaces];
-        transform.position   =  Vector3.MoveTowards(transform.position , _pointByNumberInArray.position, _float * Time.deltaTime);
+        Transform point = _points[_currentPoint];
+        transform.position = Vector3.MoveTowards(transform.position , point.position, _speed * Time.deltaTime);
 
-
-          if (transform.position == _pointByNumberInArray.position)  NextPlaceTakerLogic();
+        if (transform.position == point.position)  
+            NextPointTakerLogic();
     }
-    public Vector3 NextPlaceTakerLogic(){
-        NumberOfPlaceInArrayPlaces++;
 
-            if (NumberOfPlaceInArrayPlaces == arrayPlaces.Length)
-                NumberOfPlaceInArrayPlaces  = 0;
+    public Vector3 NextPointTakerLogic(){
+        _currentPoint++;
 
-        var thisPointVector = arrayPlaces[NumberOfPlaceInArrayPlaces].transform.position;
+        if (_currentPoint == _points.Length)
+            _currentPoint  = 0;
+
+        Vector3 thisPointVector = _points[_currentPoint].transform.position;
         transform.forward = thisPointVector - transform.position;
-        return thisPointVector;
 
-        
+        return thisPointVector;   
     }
-
-
 }
